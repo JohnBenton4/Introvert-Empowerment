5df52e5bb21af61efcb073394bff972b3589cd1e
John Benton <jtbentoniv@gmail.com>
2022-10-20 19:38:12 -0400

    fix userchallenges and userpickuplines

--- server/migrations/20221020221009-create-user-pickup-line.js
+++ server/migrations/20221020221009-create-user-pickup-line.js
@@ -1,29 +1,25 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('UserPickupLines', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
+    await queryInterface.createTable('UserPickupLines',
+      {
+        userId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'Users',
+            key: 'id',
+          },
+        },
+        pickuplineId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'PickupLines',
+            key: 'id',
+          },
+        },
       },
-      userId: {
-        type: Sequelize.INTEGER
-      },
-      pickuplineID: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('UserPickupLines');
