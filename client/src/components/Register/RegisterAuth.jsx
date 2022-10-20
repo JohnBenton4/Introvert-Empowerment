71890bfe6c0790c2c0a08a0f6779201db3302dc2
omardun <omar.rosquero10@gmail.com>
2022-10-20 19:28:57 -0400

    page layut done,  card flip animations done  and the contact form is working

--- client/src/components/Register/RegisterAuth.jsx
+++ client/src/components/Register/RegisterAuth.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function RegisterAuth() {
     return (
         <>
         <h1>Register page</h1>
+        <Link to="/login">
+            <h1>login</h1>
+        </Link>
         </>
     )
 }
--- client/src/routes/Challenges.jsx
+++ client/src/routes/Challenges.jsx
@@ -1,8 +1,14 @@
+import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar"
+import ServiceOne from "../components/Projects/ServiceOne"
 
 
 export default function Challenges() {
     return (
+        <>
         <Navbar />
+        <ServiceOne/>
+        <Footer/>
+        </>
     )
 }
