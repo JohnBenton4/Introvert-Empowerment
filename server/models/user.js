3e3bc98a2f9a127bff2a22d977841f25296f197e
John Benton <jtbentoniv@gmail.com>
2022-10-20 20:49:35 -0400

    init authentication commit

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,52 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+
+const bcrypt = require('bcrypt');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-   
-    static associate(models) {
-User.belongsToMany(models.Challenge, {
-  as: 'challenges',
-  through: models.UserChallenge,
-  forignKey: 'userID',
-  otherKey: 'challengeID',
-});   }
 
+    static associate(models) {
+      User.belongsToMany(models.Challenge, {
+        as: 'challenges',
+        through: models.UserChallenge,
+        forignKey: 'userId',
+        otherKey: 'challengeId',
+      });
+    }
+
+    static associate(models) {
+      User.belongsToMany(models.Conversation, {
+        as: 'conversations',
+        through: models.UserConversation,
+        forignKey: 'userId',
+        otherKey: 'conversationId',
+      });
+    }
+
+    static associate(models) {
+      User.belongsToMany(models.PickupLine, {
+        as: 'pickuplines',
+        through: models.UserPickupLine,
+        forignKey: 'userId',
+        otherKey: 'pickuplineId',
+      });
+    }
 
   }
-
   User.init({
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, {
+  }, 
+  {
+    hooks: {
+      async beforeCreate(user) {
+        user.password = await bcrypt.hash(user.password, 10);
+      },
+    },
     sequelize,
     modelName: 'User',
-  });
+  }
+  );
   return User;
 };
