c04862d2dfe182b9330295f60b4cbdaad7bee53d
John Benton <jtbentoniv@gmail.com>
2022-10-20 21:40:47 -0400

    init bcrypt commit

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,6 @@ const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate(models) {
       User.belongsToMany(models.Challenge, {
         as: 'challenges',
@@ -13,18 +12,14 @@ module.exports = (sequelize, DataTypes) => {
         forignKey: 'userId',
         otherKey: 'challengeId',
       });
-    }
 
-    static associate(models) {
       User.belongsToMany(models.Conversation, {
         as: 'conversations',
         through: models.UserConversation,
         forignKey: 'userId',
         otherKey: 'conversationId',
       });
-    }
 
-    static associate(models) {
       User.belongsToMany(models.PickupLine, {
         as: 'pickuplines',
         through: models.UserPickupLine,
@@ -32,21 +27,20 @@ module.exports = (sequelize, DataTypes) => {
         otherKey: 'pickuplineId',
       });
     }
-
   }
   User.init({
     email: DataTypes.STRING,
     password: DataTypes.STRING
-  }, 
-  {
-    hooks: {
-      async beforeCreate(user) {
-        user.password = await bcrypt.hash(user.password, 10);
+  },
+    {
+      hooks: {
+        async beforeCreate(user) {
+          user.password = await bcrypt.hash(user.password, 10);
+        },
       },
-    },
-    sequelize,
-    modelName: 'User',
-  }
+      sequelize,
+      modelName: 'User',
+    }
   );
   return User;
 };
--- server/server.js
+++ server/server.js
@@ -9,21 +9,57 @@ const app = express();
 const server = http.createServer(app);
 
 
-const db = require('./models/challenges');
+const {Challenge, PickupLine, Conversation, User} = require('./models');
+const user = require("./models/user");
+const jwt = require('jsonwebtoken');
 
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!');
+// app.get('/', (req, res) => {
+//     res.send('Hello from Express!');
+// });
+
+app.get('/challenges', async (req, res) => {
+    const challenges = await Challenge.findAll();
+    res.json(challenges);
 });
 
-app.get('/challenges', (req, res) => {
-    res.send('Hello from Challenges!');
+app.get('/pickuplines', async (req, res) => {
+    const pickuplines = await PickupLine.findAll();
+    res.json(pickuplines);
 });
 
+app.get('/conversations', async (req, res) => {
+    const conversations = await Conversation.findAll();
+    res.json(conversations);
+});
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
+    try {
+        const user = await User.findOne({
+            where: { email },
+        });
 
+        bcrypt.compare(password, user.password, (err, match) => {
+            if (match) {
+                const token = jwt.sign(
+                    {userId: user.id, email: user.email },
+                    process.env.JWT_SECRET,
+                    {
+                        expiresIn: '1h',
+                    }
+                );
 
+                res.json({ message: 'success', token, userId: user.is});
+            } else {
+
+            }
+        });
+    } catch {
+        res.json({ message: 'failure' });
+    }
+}),
 
 
 server.listen(port, hostname, () => {
