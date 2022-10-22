e2453d7311a392215097120a844e0a34fcca78bb
omardun <omar.rosquero10@gmail.com>
2022-10-22 10:19:29 -0400

    service 1 basic layout

--- client/src/components/Projects/ServiceOne.jsx
+++ client/src/components/Projects/ServiceOne.jsx
@@ -1,7 +1,41 @@
-export default function ServiceOne() {
+import { useState } from "react"
+import { IoCaretDown } from "react-icons/io5";
+
+
+export default function ServiceOne({ selected, setSelected }) {
+    const [isActive, setIsActive] = useState(false)
+    const options = ["Category1", "Category2", "Category3"]
     return (
-        <>
-        <h1>Service one</h1>
-        </>
+        <div className="containerService1">
+            <h1 id="title">Pick your challenege</h1>
+            <div className="socialSkills">
+                <button>Random Chaellenge</button>
+                <div className="dropdown">
+                    <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+                         {selected}
+                        <span><IoCaretDown /></span>
+                    </div>
+                    {isActive && (
+                        <div className="dropdown-content">
+                            {options.map((option) => (
+                                <div
+                                    onClick={(e) => {
+                                        setSelected(option)
+                                        setIsActive(false)
+                                    }}
+                                    className="dropdown-item"
+                                >
+                                    {option}
+                                </div>
+                            ))}
+                        </div>
+                    )}
+                </div>
+                <button>Pick up line</button>
+            </div>
+            <div id="text">
+                    <p>this is where all the text is put when a button or a category is clicked above</p>
+                </div>
+        </div>
     )
 }
--- client/src/routes/Challenges.jsx
+++ client/src/routes/Challenges.jsx
@@ -1,14 +1,17 @@
-import Footer from "../components/Footer/Footer"
+// import Footer from "../components/Footer/Footer"
 import Navbar from "../components/Navbar/Navbar"
 import ServiceOne from "../components/Projects/ServiceOne"
+import { useState } from "react"
 
 
 export default function Challenges() {
+    const [selected, setSelected] = useState("")
+
     return (
         <>
         <Navbar />
-        <ServiceOne/>
-        <Footer/>
+        <ServiceOne selected={selected} setSelected={setSelected}/>
+        {/* <Footer/> */}
         </>
     )
 }
