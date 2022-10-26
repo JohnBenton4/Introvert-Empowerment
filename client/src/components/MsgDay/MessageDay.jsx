0ee2eb4b6a4993d2e55d6003d4bee958aa664172
John Benton <jtbentoniv@gmail.com>
2022-10-25 20:09:04 -0400

    set up message of the day

--- client/src/components/MsgDay/MessageDay.jsx
+++ client/src/components/MsgDay/MessageDay.jsx
@@ -1,20 +1,52 @@
+import React from "react";
 import HomeImage from "../../images/home.jpg"
+import { useState } from "react";
+import { useEffect } from "react";
 
+const MessageDay = () => {
+    const [quotes, setQuotes] = useState([]);
 
-export default function Message() {
+    useEffect(() => {
+        getAPI()
+    }, [])
+
+    const getAPI = () => {
+        fetch("https://type.fit/api/quotes")
+
+            .then(res => res.json())
+            .then(quotes => {
+                setQuotes(quotes)
+                console.log(quotes)
+            })
+    };
+
+    const renderMessageDay = () => {
+        if (!quotes.length) {
+            return "";
+        }
+        const quote = quotes[Math.floor(Math.random() * quotes.length)];
+        return (
+            <div className="css">
+                {quote.text}
+                <br></br>
+                {quote.author}
+            </div>
+        );
+    };
+
     return (
         <div className="container">
             <div className="sub-container">
-            <img className="home-image" src={HomeImage} alt="Homeimage"/>
-            <div id="MessageDayContainer">
-            <h1>Meessage of the day....</h1>
-            <div id="message">
-                <p id="quote">"Work out your own salvation. Do not depend on others."</p>
-                <p id="author">-"Buddha"</p>
-            </div>
-            </div>
+                <img className="home-image" src={HomeImage} alt="Homeimage" />
+                <div id="MessageDayContainer">
+                    <h1>Message of the day....</h1>
+                    <div id="message">
+                        {renderMessageDay()}
+                    </div>
+                </div>
 
             </div>
         </div>
     )
 }
+export default MessageDay;
