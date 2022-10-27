c6446750e280c5107b0aa9dde0780935bd59206a
John Benton <jtbentoniv@gmail.com>
2022-10-26 23:20:41 -0400

    work on login

--- client/src/components/Login/LoginAuth.jsx
+++ client/src/components/Login/LoginAuth.jsx
@@ -1,48 +1,78 @@
-// import { useState } from 'react';
-import { Link } from "react-router-dom";
-// import userServuce from '.../.../services/userService';
+
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 
-// function Login({ onSetLoggedIn, onSetUserId }) {
-//     const [credentials, setCredentials] = useState({
-//         email: '',
-//         password: '',
-//     });
+function LoginAuth({ onSetIsLoggedIn, onSetUserId }) {
+    const [credentials, setCredentials] = useState({
+        email: '',
+        password: '',
+    });
 
-//     const navigate = useNavigate();
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setCredentials({ ...credentials, [name]: value });
+    };
 
-//     const handleChange = (event) => {
-//         const { name, value } = event.target;
+    const submit = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await
 
-//         setCredentials({ ...credentials, [name]: value });
-//     };
+                fetch('http://localhost:8000/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(credentials)
+                }).then(response => response.json())
+                    .then(result => {
+                        if (result.success === true) {
+                            const token = result.token
+                            // put the token in local storage
+                            localStorage.setItem('jsonwebtoken', token)
+                        }
+                    });
+            const { success, token, userId } = await response.json();
 
-//     const submit = async (event) => {
-//         event.preventDefault();
+            console.log(success)
 
-//         try {
-//             const response = await userService.logIn(credentials);
-//             const { message, token, userId } = await response.json();
+            if (success === 'true') {
+                localStorage.setItem('token', token);
+                onSetIsLoggedIn(true);
+                onSetUserId(userId);
+                navigate('/');
+            }
+        } catch {
+        }
+    };
 
-//             if (message === 'success') {
-//                 localStorage.setItem('token', token);
-//                 onSetIsLoggedIn(true);
-//                 onSetUserId(userId);
-//                 navigate(`/`);
-//             }
-//         } catch {
-//         }
-//     };
-// }
 
-export default function LoginAuth() {
-return (
-    <>
-        <h1>Login comp</h1>
-        <Link to="/register">
-            <h1>Register</h1>
-        </Link>
-    </>
-)
+    return (
+        <div className="App">
+            <h1>Login</h1>
+            <form onSubmit={submit}>
+                <input
+                    type="text"
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="Enter email"
+                />
+                <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    placeholder="Enter password"
+                />
+                <button  href="http://localhost:8000/" variant="primary" className="mt-3" type="submit">
+                    Log In
+                </button>
+            </form>
+        </div>
+    )
 }
+
+
+export default LoginAuth;
