b072c7f987d90ae782bf9241f51ae79c09bf02f4
John Benton <jtbentoniv@gmail.com>
2022-10-26 23:21:57 -0400

    add login and bcrypt to server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,17 @@ const port = 8000;
 const express = require("express");
 const app = express();
 const server = http.createServer(app);
-const {Challenge, PickupLine, Conversation, User} = require('./models');
-const user = require("./models/user");
+const { Challenge, PickupLine, Conversation, User } = require('./models');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+// const user = require("./models/user");
+const db = require("./models");
+
+
 
 app.use(cors())
+app.use(express.json())
+
 
 app.get('/challenges', async (req, res) => {
     const challenges = await Challenge.findAll();
@@ -26,33 +32,47 @@ app.get('/conversations', async (req, res) => {
     res.json(conversations);
 });
 
+app.get('/users', async (req, res) => {
+    const users = await User.findAll();
+    res.json(users);
+});
+
+
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
 
-    try {
-        const user = await User.findOne({
-            where: { email },
-        });
+    const email = req.body.email
+    const password = req.body.password
 
-        bcrypt.compare(password, user.password, (err, match) => {
-            if (match) {
-                const token = jwt.sign(
-                    {userId: user.id, email: user.email },
-                    process.env.JWT_SECRET,
-                    {
-                        expiresIn: '1h',
-                    }
-                );
+    const user = await db.User.findOne({
+        where: {
+            email: email
+        }
+    })
+    console.log(user)
 
-                res.json({ message: 'success', token, userId: user.is});
-            } else {
-
-            }
-        });
-    } catch {
-        res.json({ message: 'failure' });
+    if (user) {
+        const match = await bcrypt.compare(password, user.password)
+        console.log(password)
+        console.log(user.password)
+        //generate the json web token
+        if (!match) {
+            //response with not authentiated
+            
+        return res.json({ success: false, message: 'Wrong password' })
+        }
+        else {
+            const token = jwt.sign({ email: user.email }, 'SECRETKEY')
+            const result = { success: true, token: token, userId: user.id }
+            res.json(result)
+            console.log(result.success)
+        }
+    
+    } else {
+        //response with not authentiated
+        res.json({ success: false, message: 'Not a User' })
     }
-}),
+})
+
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
