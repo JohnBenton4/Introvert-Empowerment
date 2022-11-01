9254a5790080fb5801c657645cb603da3321795a
John Benton <jtbentoniv@gmail.com>
2022-10-31 21:39:08 -0400

    README updates

--- server/server.js
+++ server/server.js
@@ -16,6 +16,13 @@ app.use(cors())
 app.use(express.json())
 
 
+//authenticate will run and if it passes the test it will finally go to the function
+
+// app.get('/challenges', authenticate, async (req, res) => {
+//     const challenges = await Challenge.findAll();
+//     res.json(challenges);
+// });
+
 app.get('/challenges', async (req, res) => {
     const challenges = await Challenge.findAll();
     res.json(challenges);
