0399d96f074fed5e65e2bc1a34882fd4f1c6f25d
John Benton <jtbentoniv@gmail.com>
2022-11-08 22:38:15 -0500

    pretttier, font size

--- client/src/components/About/AboutUs.jsx
+++ client/src/components/About/AboutUs.jsx
@@ -1,13 +1,25 @@
 //we used a sentence from https://high5test.com/introvert-vs-extrovert-differences/#:~:text=3.,did%20not%20indicate%20a%20response in the "" we give credit to them"
 
 export default function AboutUs() {
-    return (
-        <div className="about-container">
-            <h1>About us</h1>
-            <p>Introvert Empowerment is an application that will help many people. According to this <a href="https://high5test.com/introvert-vs-extrovert-differences/#:~:text=3.,did%20not%20indicate%20a%20response.">article</a> 
-            "Statistically, a larger portion of the US population is introverted. The original randomized Myers-Briggs type indicator sample showed that 50.7% of Americans are introverts. Extroverts make up 49.3% of the population."
-             Being an introvert isn't a bad thing, there are just some minor setbacks that come with being an introvert. Not being able to expand your social 
-             network because you're uncomfortable with small talk and meeting new people is a challenge introverts face. Introvert Empowerment is able to help you overcome social disadvantages and help boost confidence through our powerful app. </p>
-        </div>
-    )
+  return (
+    <div className="about-container">
+      <h1>About us</h1>
+      <p>
+        Introvert Empowerment is an application that will help many people.
+        According to this{" "}
+        <a href="https://high5test.com/introvert-vs-extrovert-differences/#:~:text=3.,did%20not%20indicate%20a%20response.">
+          article
+        </a>
+        "Statistically, a larger portion of the US population is introverted.
+        The original randomized Myers-Briggs type indicator sample showed that
+        50.7% of Americans are introverts and extroverts make up 49.3% of the
+        population." Being an introvert isn't a bad thing, there are just some
+        minor setbacks that come with being an introvert. Not being able to
+        expand your social network because you're uncomfortable with small talk
+        and meeting new people is a challenge that the majority of American's face. Our app, Introvert
+        Empowerment, is able to help you overcome social disadvantages and help
+        boost confidence.{" "}
+      </p>
+    </div>
+  );
 }
