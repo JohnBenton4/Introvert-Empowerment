06778d6b5ba27df6634e086ba912d76e5ff6e71e
John Benton <jtbentoniv@gmail.com>
2022-12-01 18:25:02 -0500

    bild to gitignore, add engines

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-// const hostname = "127.0.0.1";
-// const port = 8080;
+const hostname = "127.0.0.1";
+const port = 8080;
 // const server = http.createServer(app);
-// const user = require("./models/user");
-// const bcrypt = require('bcrypt');
-// const db = require("./models");
-// const jwt = require('jsonwebtoken');
+const user = require("./models/user");
+const bcrypt = require('bcrypt');
+const db = require("./models");
+const jwt = require('jsonwebtoken');
 // const app = express();
 
 const http = require("http");
@@ -19,9 +19,7 @@ server.use(cors());
 server.use(express.json());
 server.use(express.static(path.resolve(`${__dirname}/client/build`)));
 
-server.get("*", (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}/client/build/index.html`));
-});
+
 
 //authenticate will run and if it passes the test it will finally go to the function
 
@@ -44,6 +42,46 @@ server.get("/users", async (req, res) => {
   const users = await User.findAll();
   res.json(users);
 });
+
+server.post('/login', async (req, res) => {
+
+    const email = req.body.email
+    const password = req.body.password
+
+    const user = await db.User.findOne({
+        where: {
+            email: email
+        }
+    })
+    console.log(user)
+
+    if (user) {
+        const match = await bcrypt.compare(password, user.password)
+        console.log(password)
+        console.log(user.password)
+        //generate the json web token
+        if (!match) {
+            //response with not authenticated
+
+        return res.json({ success: false, message: 'Wrong password' })
+        }
+        else {
+            const token = jwt.sign({ email: user.email }, 'SECRETKEY')
+            const result = { success: true, token: token, userId: user.id }
+            res.json(result)
+            console.log(result.success)
+        }
+
+    } else {
+        //response with not authenticated
+        res.json({ success: false, message: 'Not a User' })
+    }
+});
+
+
+server.get("*", (req, res) => {
+    res.sendFile(path.resolve(`${__dirname}/client/build/index.html`));
+  });
 
 // app.get('/challenges', async (req, res) => {
 //     const challenges = await Challenge.findAll();
@@ -69,40 +107,7 @@ server.listen (8080, () => {
   console.log("The server is listening at PORT 8080.");
 });
 
-// app.post('/login', async (req, res) => {
 
-//     const email = req.body.email
-//     const password = req.body.password
-
-//     const user = await db.User.findOne({
-//         where: {
-//             email: email
-//         }
-//     })
-//     console.log(user)
-
-//     if (user) {
-//         const match = await bcrypt.compare(password, user.password)
-//         console.log(password)
-//         console.log(user.password)
-//         //generate the json web token
-//         if (!match) {
-//             //response with not authenticated
-
-//         return res.json({ success: false, message: 'Wrong password' })
-//         }
-//         else {
-//             const token = jwt.sign({ email: user.email }, 'SECRETKEY')
-//             const result = { success: true, token: token, userId: user.id }
-//             res.json(result)
-//             console.log(result.success)
-//         }
-
-//     } else {
-//         //response with not authenticated
-//         res.json({ success: false, message: 'Not a User' })
-//     }
-// })
 
 // server.listen(port, hostname, () => {
 //     console.log(`Server running at http://${hostname}:${port}/`);
