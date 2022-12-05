665f8b29f792e886b9bd3d17b6f4ce7ebaa3d1ef
John Benton <jtbentoniv@gmail.com>
2022-12-04 19:43:52 -0500

    port fix?

--- models/index.js
+++ models/index.js
@@ -5,14 +5,12 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-// const env = "development";
-// const env = "production";
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (!!config.use_env_variable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
